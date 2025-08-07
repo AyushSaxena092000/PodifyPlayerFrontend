bff70dad193e0622ee34474dc9d8753567101286
AyushSaxena092000 <ayushsaxena092000@gmail.com>
2025-08-07 23:40:01 +0530

    frontend start

---
+++ src/views/auth/SignUp.tsx
@@ -0,0 +1,118 @@
+
+import {FC, useState} from 'react';
+import {
+  Button,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import * as yup from 'yup';
+import Form from '../../components/form';
+import AuthInputField from '../../components/form/AuthInputField';
+import AppLink from '../../ui/AppLink';
+import SubmitBtn from '../../components/form/SubmitBtn';
+import PasswordVisibilityIcon from '../../ui/PasswordVisibilityIcon';
+import AuthFormContainer from '../../components/AuthFormContainer';
+
+const signupSchema = yup.object({
+  name: yup
+    .string()
+    .trim('Name is missing!')
+    .min(3, 'Invalid name!')
+    .required('Name is required!'),
+  email: yup
+    .string()
+    .trim('Email is missing!')
+    .email('Invalid email!')
+    .required('Email is required!'),
+  password: yup
+    .string()
+    .trim('Password is missing!')
+    .min(8, 'Password is too short!')
+    .matches(
+      /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#\$%\^&\*])[a-zA-Z\d!@#\$%\^&\*]+$/,
+      'Password is too simple!',
+    )
+    .required('Password is required!'),
+});
+
+interface Props {}
+
+const initialValues = {
+  name: '',
+  email: '',
+  password: '',
+};
+
+const SignUp: FC<Props> = props => {
+  const [secureEntry, setSecureEntry] = useState(true);
+
+  const togglePasswordView = () => {
+    setSecureEntry(!secureEntry);
+  };
+
+  return (
+    <Form
+      onSubmit={values => {
+        console.log(values);
+      }}
+      initialValues={initialValues}
+      validationSchema={signupSchema}>
+      <AuthFormContainer
+        heading="Welcome!"
+        subHeading="Let's get started by creating your account.">
+        <View style={styles.formContainer}>
+          <AuthInputField
+            name="name"
+            placeholder="John Doe"
+            label="Name"
+            containerStyle={styles.marginBottom}
+          />
+          <AuthInputField
+            name="email"
+            placeholder="john@email.com"
+            label="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            containerStyle={styles.marginBottom}
+          />
+          <AuthInputField
+            name="password"
+            placeholder="********"
+            label="Password"
+            autoCapitalize="none"
+            secureTextEntry={secureEntry}
+            containerStyle={styles.marginBottom}
+            rightIcon={<PasswordVisibilityIcon privateIcon={secureEntry} />}
+            onRightIconPress={togglePasswordView}
+          />
+          <SubmitBtn title="Sign up" />
+
+          <View style={styles.linkContainer}>
+            <AppLink title="I Lost My Password" />
+            <AppLink title="Sign in" />
+          </View>
+        </View>
+      </AuthFormContainer>
+    </Form>
+  );
+};
+
+const styles = StyleSheet.create({
+  formContainer: {
+    width: '100%',
+  },
+  marginBottom: {
+    marginBottom: 20,
+  },
+  linkContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+});
+
+export default SignUp;
